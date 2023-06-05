d116a2c7a350a159e965519420c34ddaea43af19
misternooblet <dragonroadsupport@gmail.com>
2023-06-05 16:12:09 +0300

    login

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,70 +1,75 @@
-import { postData } from "@/utilities/serverRequests/serverRequests";
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import { postData } from '@/utilities/serverRequests/serverRequests';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 interface IGoogleProvider {
   clientId: string;
   clientSecret: string;
-  authorization: IAuthorizationOptions
+  authorization: IAuthorizationOptions;
 }
 
 interface IProviders {
   GoogleProvider: IGoogleProvider;
-} 
+}
 
 interface IAuthorizationOptions {
   params: {
-    prompt: string,
-    access_type: string,
-    response_type: string,
-    scope: string
-  }
+    prompt: string;
+    access_type: string;
+    response_type: string;
+    scope: string;
+  };
 }
 
 const AuthorizationParams: IAuthorizationOptions = {
   params: {
-    prompt: "consent",
-    access_type: "offline",
-    response_type: "code",
-    scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar"
-  }
-}
+    prompt: 'consent',
+    access_type: 'offline',
+    response_type: 'code',
+    scope:
+      'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+  },
+};
 
 const googleProviderOptions: IGoogleProvider = {
   clientId: process.env.NEXT_PUBLIC_GOOGLE_ID as string,
   clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET as string,
-  authorization: AuthorizationParams
-}
+  authorization: AuthorizationParams,
+};
 
 const authOptions: IProviders = {
-  GoogleProvider: googleProviderOptions
+  GoogleProvider: googleProviderOptions,
 };
 
 const handler = NextAuth({
-  providers: [
-    GoogleProvider(authOptions.GoogleProvider),
-  ],
+  providers: [GoogleProvider(authOptions.GoogleProvider)],
   session: {
     strategy: 'jwt',
   },
   callbacks: {
     async signIn(user) {
-      const { email } = user.user
-      if(user.account) {
-        const { provider, access_token, expires_at, refresh_token } = user.account
-        await postData('/integration', {token: access_token, refreshToken: refresh_token, expiresAt: expires_at, userEmail: email, provider})
-      }    
-      return true
+      const { email } = user.user;
+      if (user.account) {
+        const { provider, access_token, expires_at, refresh_token } = user.account;
+        await postData('/integration', {
+          token: access_token,
+          refreshToken: refresh_token,
+          expiresAt: expires_at,
+          userEmail: email,
+          provider,
+        });
+      }
+      return true;
     },
 
     async session({ session }) {
-      return session
+      return session;
     },
 
     redirect({ url, baseUrl }) {
       return baseUrl;
     },
-  }
+  },
 });
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,38 +1,41 @@
-"use client";
+'use client';
 
-import { getData, postData } from "@/utilities/serverRequests/serverRequests"
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { getData, postData } from '@/utilities/serverRequests/serverRequests';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
-  const data = useSession()
-  console.log('data', data)
-  const [loggedUser, setLoggedUser] = useState(null)
+  const data = useSession();
+  const [loggedUser, setLoggedUser] = useState(null);
 
-  // fix infinite loop
   useEffect(() => {
     const getUser = async () => {
-      if (loggedUser) return // break infinite loop by checking if a user already logged in
-      const user = await getData("/auth/me")
-      setLoggedUser(user)
-    }
-    if(data.data?.user && !loggedUser) {
-      const { email, name } = data.data?.user
-      postData('/auth/register', {email, name})
-    }
-    getUser()
-  }, [data.data?.user, loggedUser])
+      if (loggedUser) return; // Break infinite loop by checking if a user is already logged in
+      const user = await getData('/auth/me');
+      setLoggedUser(user);
+    };
+
+    const registerUser = async () => {
+      if (data.data?.user && !loggedUser) {
+        const { email, name } = data.data?.user;
+        await postData('/auth/register', { email, name });
+        getUser(); // Call getUser after registering the user
+      }
+    };
+
+    const initialize = async () => {
+      await registerUser(); // Register the user and call getUser after registration
+    };
+
+    initialize();
+  }, [data.data?.user, loggedUser]);
 
   return (
     <div>
-      {loggedUser ? (
-        <p>user is logged in</p>
-      ) : (
-        <p>no user logged in</p>
-      )}
-  <h1>Home Page</h1>
+      {loggedUser ? <p>user is logged in</p> : <p>no user logged in</p>}
+      <h1>Home Page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
