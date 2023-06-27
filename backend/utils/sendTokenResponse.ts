eb53b56e64c385b188c2a1166132475f626dcb7e
misternooblet <dragonroadsupport@gmail.com>
2023-06-27 21:08:22 +0300

    maybe now?

--- backend/utils/sendTokenResponse.ts
+++ backend/utils/sendTokenResponse.ts
@@ -7,7 +7,7 @@ interface TokenOptions {
   httpOnly: boolean;
   secure?: boolean;
   domain?: string;
-  sameSite?: string;
+  sameSite?: 'lax' | 'strict' | 'none';
 }
 
 // Get token from model, create cookie and send response
@@ -15,15 +15,15 @@ const sendTokenResponse = (user: User, statusCode: number, res: Response) => {
   // Create token
   const token = getSignedJwtToken(user.hash);
 
-  const options = {
+  const options: TokenOptions = {
     expires: new Date(
       // Convert the 30 days in the config to 30 days in milliseconds
       Date.now() + (process.env.JWT_COOKIE_EXPIRE ? parseInt(process.env.JWT_COOKIE_EXPIRE) : 30) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    domain: 'cortex-dev-api.onrender.com',
-    sameSite: false,
+    domain: '.onrender.com',
+    sameSite: 'none',
   };
 
   // It's up to the client-side to decide how to handle the token
