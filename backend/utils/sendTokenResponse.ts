1abd7fe9592ba4d0b1f7dca7bd56313c247f23c5
misternooblet <dragonroadsupport@gmail.com>
2023-06-27 20:46:26 +0300

    test

--- backend/utils/sendTokenResponse.ts
+++ backend/utils/sendTokenResponse.ts
@@ -15,7 +15,7 @@ const sendTokenResponse = (user: User, statusCode: number, res: Response) => {
   // Create token
   const token = getSignedJwtToken(user.hash);
 
-  const options: TokenOptions = {
+  const options = {
     expires: new Date(
       // Convert the 30 days in the config to 30 days in milliseconds
       Date.now() + (process.env.JWT_COOKIE_EXPIRE ? parseInt(process.env.JWT_COOKIE_EXPIRE) : 30) * 24 * 60 * 60 * 1000
@@ -23,7 +23,7 @@ const sendTokenResponse = (user: User, statusCode: number, res: Response) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
     domain: '.onrender.com',
-    sameSite: 'none',
+    sameSite: false,
   };
 
   // It's up to the client-side to decide how to handle the token
