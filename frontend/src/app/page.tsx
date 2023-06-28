4f903d492049e6493b2d97914a0be31a191e195f
Or Rodrigez <orrodrigez1@gmail.com>
2023-06-28 16:41:15 +0300

    Comment out the redirect for logged users

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client'
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useGlobalContext } from "./context/store";
 
 const HomePage = () => {
   const { user } = useGlobalContext()
 
-  useEffect(() => {
-    if (user?.hash) {
-      console.log('user:',user)
-      // window.location.href = '/dashboard'
-    }
-  }, [user])
+  // useEffect(() => {
+  //   if (user?.hash) {
+  //     console.log('user:',user)
+  //     // window.location.href = '/dashboard'
+  //   }
+  // }, [user])
   return (
     <div>
-      <h1>Welcome to cortex!</h1>
+      <h1>Welcome to cortex {user?.name ? user.name : ''}!</h1>
       <p>This page is a work in progress, but you can still use our application!</p>
       <p>Click the user icon in the top right to register or login.</p>
     </div>
