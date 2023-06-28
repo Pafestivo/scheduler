4732af4a868dd451713611be23029a9aef2fdbf9
Or Rodrigez <orrodrigez1@gmail.com>
2023-06-28 12:06:45 +0300

    Redirect logged user from home to dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,20 @@
+'use client'
+import { useEffect } from "react";
+import { useGlobalContext } from "./context/store";
+
 const HomePage = () => {
+  const { user } = useGlobalContext()
+
+  useEffect(() => {
+    if (user) {
+      window.location.href = '/dashboard'
+    }
+  }, [user])
   return (
     <div>
       <h1>Welcome to cortex!</h1>
       <p>This page is a work in progress, but you can still use our application!</p>
+      <p>Click the user icon in the top right to register or login.</p>
     </div>
   );
 };
