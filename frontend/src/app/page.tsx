9e8f988e650bfbcd585e3ed2117f32882d488b3d
Or Rodrigez <orrodrigez1@gmail.com>
2023-07-02 17:12:26 +0300

    Add a redirect from home page to dashboard for logged users

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,30 @@
 'use client'
+import { useEffect } from "react";
 import { useGlobalContext } from "./context/store";
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
-  const { user } = useGlobalContext()
+  const { user } = useGlobalContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if(user) {
+      router.push('/dashboard');
+    }
+  }, [router, user])
 
   return (
     <div>
       <h1>Welcome to cortex {user?.name ? user.name : ''}!</h1>
       <div>
-        <p>This page is a work in progress, but you can still use our application!</p>
-        <p>Click the user icon in the top right to register or login.</p>
+        {user?.name ? (
+          <h1>Redirecting to your dashboard.</h1>
+        ) : (
+          <div>
+            <p>This page is a work in progress, but you can still use our application!</p>
+            <p>Click the user icon in the top right to register or login.</p>
+          </div>
+        )}
       </div>
     </div>
   );
