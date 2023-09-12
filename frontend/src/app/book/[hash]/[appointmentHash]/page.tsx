b036398c78d63620cd3d153cf55a4357e166be9e
Or Rodrigez <orrodrigez1@gmail.com>
2023-09-12 18:08:12 +0300

    Fix build issue(type notations)

--- frontend/src/app/book/[hash]/[appointmentHash]/page.tsx
+++ frontend/src/app/book/[hash]/[appointmentHash]/page.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import CalendarComponent from "@/components/CalendarComponent";
+import { getData } from "@/utilities/serverRequests/serverRequests";
+import { calendar } from "@/utilities/types";
 
-const reschedulePage = ({
+const reschedulePage = async ({
   params,
 }: {
   params: { hash: string; appointmentHash: string };
 }) => {
-  return (
+  let calendar: { success: boolean; data: calendar } | null = null;
+  try {
+    calendar = await getData(`/calendars/single/${params.hash}`);
+  } catch (error) {
+    console.log("error getting calendar", error);
+  }
+  return calendar ? (
     <div>
       <h1>Reschedule page</h1>
       <CalendarComponent
-        calendarHash={params.hash}
+        calendar={calendar.data}
         appointmentHash={params.appointmentHash}
       />
     </div>
-  );
+  ) : null;
 };
 
 export default reschedulePage;
