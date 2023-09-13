196d70cf1a1255bdc93961ca2c89617f2dfbb9ed
Or Rodrigez <orrodrigez1@gmail.com>
2023-09-13 16:50:27 +0300

    Add theme to reschedule page

--- frontend/src/app/book/[hash]/[appointmentHash]/page.tsx
+++ frontend/src/app/book/[hash]/[appointmentHash]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CalendarComponent from "@/components/CalendarComponent";
 import { getData } from "@/utilities/serverRequests/serverRequests";
-import { calendar } from "@/utilities/types";
+import { calendar, theme } from "@/utilities/types";
 
 const reschedulePage = async ({
   params,
@@ -9,17 +9,20 @@ const reschedulePage = async ({
   params: { hash: string; appointmentHash: string };
 }) => {
   let calendar: { success: boolean; data: calendar } | null = null;
+  let theme: { success: boolean; data: theme } | null = null;
   try {
     calendar = await getData(`/calendars/single/${params.hash}`);
+    theme = await getData(`/themes/${calendar?.data.activeTheme}`);
   } catch (error) {
     console.log("error getting calendar", error);
   }
-  return calendar ? (
+  return calendar && theme ? (
     <div>
       <h1>Reschedule page</h1>
       <CalendarComponent
         calendar={calendar.data}
         appointmentHash={params.appointmentHash}
+        theme={theme.data}
       />
     </div>
   ) : null;
